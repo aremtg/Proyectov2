538c047dbeea1415ddeb645f39514cf18897ea24
aremtg <tgz57031@gmail.com>
2024-03-01 08:58:53 -0500

    img

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,19 @@
 <footer>
-<span class="ssaci-cont">SSACI</span><img width="300px" class="imgFooter" src="images/ministerio-trabajo.svg" alt="">
+    <div class="footer-content">
+        <span class="ssaci-cont">SSACI</span>
+        <img width="300px" class="imgFooter" src="images/ministerio-trabajo.svg" alt="">
+    </div>
+    <div class="footer-social">
+        <ul>
+            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+        </ul>
+    </div>
+    <div class="footer-info">
+        <p>Dirección: Calle 36, Cra 19, Yopal</p>
+        <p>Teléfono: +57 3234545667</p>
+        <p>Email: ssacisena@example.com</p>
+    </div>
 </footer>
